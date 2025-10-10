d80720767fc23edd06a4243d643e27eb5760e96d
howard19681223 <howard681223@gmail.com>
2025-10-10 09:51:53 +0800

    更新 PackageShowInfo.java

--- app/src/main/java/com/minhui/networkcapture/PackageShowInfo.java
+++ app/src/main/java/com/minhui/networkcapture/PackageShowInfo.java
@@ -7,7 +7,7 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Parcel;
 import android.os.Parcelable;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 
 import java.io.Serializable;
 import java.util.ArrayList;
