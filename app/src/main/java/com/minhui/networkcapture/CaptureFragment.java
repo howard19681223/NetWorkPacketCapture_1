1f0392f299b1b915199f5ab6caa01fc77f8c1067
howard19681223 <howard681223@gmail.com>
2025-10-10 09:53:57 +0800

    更新 CaptureFragment.java

--- app/src/main/java/com/minhui/networkcapture/CaptureFragment.java
+++ app/src/main/java/com/minhui/networkcapture/CaptureFragment.java
@@ -5,7 +5,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
