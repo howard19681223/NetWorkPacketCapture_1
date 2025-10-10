964185eeec082f6144446920634a0a1558080c78
howard19681223 <howard681223@gmail.com>
2025-10-10 09:09:16 +0800

    更新 ThreadProxy.java

--- vpnadaptercore/src/main/java/com/minhui/vpn/utils/ThreadProxy.java
+++ vpnadaptercore/src/main/java/com/minhui/vpn/utils/ThreadProxy.java
@@ -1,6 +1,8 @@
 package com.minhui.vpn.utils;
 
-import android.support.annotation.NonNull;
+// import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.LinkedBlockingQueue;
